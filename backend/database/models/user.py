d119167f357b093941234fdebc1441a2d941079d
Jhonnathan Ocampo <jsocampod@eafit.edu.co>
2025-03-08 16:36:14 -0500

    imp: alembic and sqlalchemy configuration

---
+++ backend/database/models/user.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+from .base import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(length=255))
+    email = Column(String(length=255), unique=True, index=True)
+    password = Column(String(length=255))
+    role = Column(String(length=50))
+    createdAt = Column(DateTime)
+    updatedAt = Column(DateTime)
+
+    # Relationships
+    dataSet = relationship("DataSet", back_populates="user", uselist=False)
+    chats = relationship("Chat", back_populates="user")
+    alerts = relationship("Alert", back_populates="user")
