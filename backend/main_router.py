cc3f2a497e7cf5f816557038c4177f5780d8376c
Santiago Neusa <sneusar@gmail.com>
2025-04-21 12:16:17 -0500

    fix: merge

--- backend/main_router.py
+++ backend/main_router.py
@@ -1,17 +1,19 @@
 from fastapi import APIRouter
 
+from apps.auth.routes import router as auth_router
 from apps.chat.routes import router as chat_router
 from apps.dashboard.routes import router as dashboard_router
+from apps.detail_report.routes import router as detail_report_router
 from apps.file_manager.routes import router as file_manager_router
 from apps.synthetic_data.routes import router as synthetic_data_router
-from apps.auth.routes import router as auth_router
 from apps.user.routes import router as user_router
 
 router = APIRouter()
 
 router.include_router(auth_router, prefix="/auth", tags=["auth"])
-router.include_router(user_router, prefix="/users", tags=["users"])
 router.include_router(chat_router, prefix="/chats", tags=["chat"])
 router.include_router(dashboard_router, prefix="/dashboard", tags=["dashboard"])
-router.include_router(synthetic_data_router, prefix="/synthetic_data", tags=["synthetic_data"])
+router.include_router(detail_report_router, prefix="/detail_report", tags=["detail_report"])
 router.include_router(file_manager_router, prefix="/file_manager", tags=["file_manager"])
+router.include_router(synthetic_data_router, prefix="/synthetic_data", tags=["synthetic_data"])
+router.include_router(user_router, prefix="/users", tags=["users"])
