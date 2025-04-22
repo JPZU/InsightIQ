76b619c3d0fbaa0f3014ace07fba09d96b754ccd
SaraCmque <saramanrique2016@gmail.com>
2025-04-21 20:12:53 -0500

    test for alarms created

---
+++ frontend/src/services/AlarmService.ts
@@ -0,0 +1,34 @@
+import axios from 'axios'
+import { BaseService } from '@/services/BaseService'
+
+class AlarmService extends BaseService {
+  private static readonly BASE_URL = `${import.meta.env.VITE_API_URL}/alarm_management`
+
+  static async listAlarms() {
+    const response = await axios.get(`${this.BASE_URL}/list`)
+    return response.data
+  }
+
+  static async deleteAlarm(id: number) {
+    return await axios.delete(`${this.BASE_URL}/delete/${id}`)
+  }
+
+  static async createAlarm(userInput: string) {
+    return await axios.post(`${this.BASE_URL}/create`, null, {
+      params: { user_input: userInput },
+    })
+  }
+
+  static async updateAlarm(id: number, updatedData: any) {
+    return await axios.patch(`${this.BASE_URL}/update/${id}`, updatedData)
+  }
+
+  static async checkAlarms() {
+    return await axios.get(`${this.BASE_URL}/check_alarm`, {
+      params: { table_name: 'inventario' }
+    })
+  }
+  
+}
+
+export default AlarmService
