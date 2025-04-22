b3118fb930f16cc384d5e054b873136042cf16fb
SaraCmque <saramanrique2016@gmail.com>
2025-04-22 17:06:30 -0500

    db.sqlite created again, new version adde

--- backend/utils/alarm_manager.py
+++ backend/utils/alarm_manager.py
@@ -64,9 +64,12 @@ class AlarmManager:
 
     def insert_alarm(self, alarm_details: dict):
         query = text("""
-            INSERT INTO alerts (`condition`, `table_name`, `field`, `threshold`, `description`, createdAt, updatedAt, user_id)
-            VALUES (:condition, :table_name, :field, :threshold, :description, :createdAt, :updatedAt, :user_id)
+            INSERT INTO alerts (`condition`, `table_name`, `field`, `threshold`, `description`, createdAt, updatedAt, user_id, is_active)
+            VALUES (:condition, :table_name, :field, :threshold, :description, :createdAt, :updatedAt, :user_id, :is_active)
         """)
+        
+        is_active = alarm_details.get("is_active", True)  # By default, set to True
+        
         with engine.connect() as conn:
             conn.execute(query, {
                 "condition": alarm_details["condition"],
@@ -76,10 +79,11 @@ class AlarmManager:
                 "description": alarm_details["description"],
                 "createdAt": alarm_details["createdAt"],
                 "updatedAt": alarm_details["updatedAt"],
-                "user_id": alarm_details["user_id"]
+                "user_id": alarm_details["user_id"],
+                "is_active": is_active  
             })
             conn.commit()
-        
+            
     def evaluate_alarm(self, table_name: str):
         query = text("""
             SELECT * FROM alerts where table_name = :table_name
